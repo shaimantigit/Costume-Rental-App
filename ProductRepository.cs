32fda6c40b7fd359fcf09323f39241825487c2e8
shaimantigit <120580422+shaimantigit@users.noreply.github.com>
2023-05-23 15:55:43 +0530

    Add files via upload

---
+++ ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using FinalMockProject.DAL.Interface;
+using FinalMockProject.Data;
+using FinalMockProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalMockProject.DAL
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
