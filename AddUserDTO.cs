6616ea528fda8cb3c707d66fb2faced788e75553
shaimantigit <120580422+shaimantigit@users.noreply.github.com>
2023-05-23 16:05:42 +0530

    Add files via upload

---
+++ AddUserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalMockProject.Models.DTO
+{
+    public class AddUserDTO
+    {
+        public int User_Id { get; set; }
+        [Required]
+        public string User_Name { get; set; }
+        [Required]
+       // [MaxLength(100, ErrorMessage = "Password shoild have maximum  10 characters.")]
+        public string User_Password { get; set; }
+        [Required]
+        //[MaxLength(10, ErrorMessage = "Contact Number must be of 10 digits.")]
+        public int User_Contact { get; set; }
+        [Required]
+
+        public string User_Email { get; set; }
+        [Required]
+        public string Home_Address { get; set; }
+        [Required]
+        public int RoleId { get; set; }
+    }
+}
---
+++ OrderDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FinalMockProject.Models.DTO
+{
+    public class OrderDTO
+    {
+        public int Order_Id { get; set; }
+        public double TotalAmount { get; set; }
+        public DateTime DateOfRent { get; set; }
+        public DateTime Return_Date { get; set; }
+        public Boolean Is_Returned { get; set; }
+        public int User_Id { get; set; }
+
+    }
+}
