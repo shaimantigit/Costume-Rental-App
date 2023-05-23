9f4f11f9aa2a6bb8bda31740eb4dde195c5d149f
shaimantigit <120580422+shaimantigit@users.noreply.github.com>
2023-05-23 15:57:08 +0530

    Add files via upload

---
+++ Repository.cs
@@ -0,0 +1,49 @@
+﻿using FinalMockProject.DAL.Interface;
+using FinalMockProject.Data;
+using FinalMockProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalMockProject.DAL
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _entities;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _entities = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _entities.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _entities.FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _entities.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _entities.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+       
+
+    }
+}
