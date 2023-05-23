863aa76342c4003de41dce631d0ee2a6e408dab7
shaimantigit <120580422+shaimantigit@users.noreply.github.com>
2023-05-23 15:55:20 +0530

    Add files via upload

---
+++ OrdersRepository.cs
@@ -0,0 +1,17 @@
+﻿using FinalMockProject.DAL.Interface;
+using FinalMockProject.Data;
+using FinalMockProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalMockProject.DAL
+{
+    public class OrdersRepository : Repository<Order>, IOrdersRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public OrdersRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
+
