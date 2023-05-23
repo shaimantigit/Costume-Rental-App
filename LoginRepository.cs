136597b003b2221e7a8211af65ab7e1fbe0d1432
shaimantigit <120580422+shaimantigit@users.noreply.github.com>
2023-05-23 15:54:58 +0530

    Add files via upload

---
+++ LoginRepository.cs
@@ -0,0 +1,20 @@
+﻿using FinalMockProject.DAL.Interface;
+using FinalMockProject.Data;
+using FinalMockProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalMockProject.DAL
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public LoginRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+            public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _context.Users.Include("Roles").FirstOrDefaultAsync(x => x.User_Email == email);
+        }
+    }
+}
